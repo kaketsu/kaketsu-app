0eaf40240d2619781f8b5eaf0dc8c45682721dcc
kaketsu <huajiewuxi@gmail.com>
2016-01-29 18:18:02 +0800

    add showEs6

---
+++ assets/js.es6/showEs6.es6.js
@@ -0,0 +1,26 @@
+var a = [];
+for (let i = 0; i < 10; i++) {
+  a[i] = function () {
+    console.log(i);
+  };
+}
+a[6](); 
+
+
+
+function test1(){
+  return new Promise(function(){
+  	setTimeout(function(){
+  		console.log('test');
+    	resolve('test1');
+  	},3000)
+    
+  });
+}
+
+const fn = async function(){
+  await test1();
+  console.log('hello world');
+}
+
+fn();
